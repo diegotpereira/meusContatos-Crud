35ebb8a335d009da8d69783009ee68b9d1b0fc64
T.I <T.I@TI.rsatacadao.local>
2020-11-06 14:24:42 -0300

    alteração no arquivo classe ContatoDAO.java adicionado metodos de acesso

---
+++ meusContatos-Crud/src/main/java/br/com/java/dao/ContatoDAO.java
@@ -0,0 +1,57 @@
+package br.com.java.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.java.factory.ConexaoBD;
+import br.com.java.model.Contato;
+
+
+
+public class ContatoDAO {
+	
+	EntityManager entity = ConexaoBD.getEntityManagerFactory().createEntityManager();
+	
+	public void salvar ( Contato contato) {
+		
+		entity.getTransaction().begin();
+		entity.persist(contato);
+		entity.getTransaction().commit();
+	}
+	
+	public void editar(Contato contato) {
+		
+		entity.getTransaction().begin();
+		entity.persist(contato);
+		entity.getTransaction().commit();
+	}
+	
+	public Contato buscar (int idContato) {
+		Contato contato = new Contato();
+		contato = entity.find(Contato.class, idContato);
+		return contato;
+		
+	}
+	
+	public void deletar(int idContato) {
+		
+		Contato contato = new Contato();
+		contato = entity.find(Contato.class, idContato);
+		entity.getTransaction().begin();
+		entity.remove(contato);
+		entity.getTransaction().commit();
+	}
+	
+	public List <Contato> listarContatos(){
+		List<Contato> listaContatos = new ArrayList<Contato>();
+		Query result = entity.createQuery("SELECT contato FROM Contato contato");
+		listaContatos = result.getResultList();
+				
+		return listaContatos;
+		
+	}
+
+}
